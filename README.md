d92fff84e5105d6cff59388dc38b564f2f730d80
Michael Keating <humanoidanalog@gmail.com>
2014-07-16 15:00:37 -0700

    magic

--- README.md
+++ README.md
@@ -1,4 +1,55 @@
-sailor
-======
+# Sailor
 
-cowsay-style sailor moon program!
+Install: 
+```bash
+npm install -g sailor
+```
+
+Run:
+```bash
+sailor "some message"
+```
+
+Piped:
+```bash
+fortune | sailor # or any other echoing program that pipes
+```
+
+Help:
+```bash
+sailor -h
+sailor -help
+sailor --help
+```
+
+More art:
+```bash
+sailor -a chibi
+```
+possible options:
+- chibi
+- haruka
+- jupiter
+- luna
+- makoto
+- mars1
+- mars2
+- mercury
+- michiru
+- moon1
+- moon2
+- neptune
+- rei
+- scout
+- uranus
+- usagi1
+- usagi2
+- usagi3
+- venus
+
+## License
+
+Code is MIT/BSD 2 Clause dual licensed.
+
+Art is copyright Project Anime Online, and can be found originally at
+http://projectanime.com/sailormoon/sm-ascii.htm
--- index.js
+++ index.js
@@ -1,30 +1,70 @@
 #!/usr/bin/env node
+process.stdin.setEncoding('utf8')
 
 var wrap = require('word-wrap')
-  , argv = process.argv
-  , stdin = process.stdin
+  , path = require('path')
+  , fs = require('fs')
 
-// just pretend this is a "monad"
-if (argv.length > 2) {
-  if (argv[2] === '-h' || argv[2] === '-help' || argv[2] === '--help')
-    process.stdout.write('Usage: "$0 text", or piped: "textprogram | $0"\n'
-      + '  Art is copyright by Project Anime Online! This derivative work is '
-      + 'for\n  educational purposes only, and is protected by fair use. '
-      + 'Sailor moon belongs to\n  its creators, I hold no copyright! '
-      + 'You can find the original art here:\n    '
-      + 'http://projectanime.com/sailormoon/sm-ascii.htm\n')
-  else print(argv.slice(2, argv.length).join(' '))
-} else {
-  var data = []
-  stdin.setEncoding('utf8')
-  stdin.on('readable', function() {
-    var chunk = stdin.read()
-    if (chunk !== null) print(chunk) 
-    else process.exit(1)
-  })
+  , help = 'Usage: "sailor blah blah...", or piped: "textprogram | sailor"\n\n'
+    + '  Art is copyright by Project Anime Online! This derivative work is '
+    + 'for\n  educational purposes only, and is protected by fair use. '
+    + 'Sailor moon belongs to\n  its creators, I hold no copyright! '
+    + '\n\nYou can find the original art here:\n  '
+    + 'http://projectanime.com/sailormoon/sm-ascii.htm\n\n'
+
+  , main = 'moon1'
+
+// implemented as a pure function, for fun and profit
+process.stdin.on('readable', function () { 
+  process.stdout.write(
+    process.argv.length > 2 ? ( // are there cli args
+      ( process.argv[2] === '-h' 
+        || process.argv[2] === '-help' 
+        || process.argv[2] === '--help' ) ? 
+      help : ( 
+        process.argv[2] === '-a' ? (
+          process.argv.length > 4 ?
+          format(
+            process.argv.slice(4, process.argv.length).join(' ')
+          , process.argv[3]
+          ) : format(process.stdin.read(), process.argv[3])
+        ) : format(process.argv.slice(2, process.argv.length).join(' '), main)
+      )
+    ) : (  // or a pipe
+      format(process.stdin.read(), main) 
+    )
+  )
+})
+
+// formats text into array of text lines and parses it thru "style", which creates art
+// "null" is process.stdin.read's EOF value, run a newline with it!
+function format (text, art) { 
+  return text === null ? '\n' : style(
+    [Array(46).join('_')].concat(
+      wrap(text.replace(/\s+/g, ' '), { width: 40 })
+      .split('\n')
+      .map(function(string){ 
+        return '| ' + string.replace(/^(\s*)|(\s*)$/g, '') 
+          + Array(44 - string.length).join(' ') + ' |'
+      })
+    , Array(46).join('-')
+    )
+  , load(art).split('\n')
+  )
 }
 
-function print (data) { 
-  console.log(wrap(data, { width: 40 }).split('\n'))
-  process.exit(0)
+// takes array of lines and inserts it into ultra cute text art
+function style (lines, art) {
+  return art.map(function (line, index) {
+    return line 
+      + Array(80 - line.length).join(' ') 
+      + (lines[index - 5] !== undefined ? lines[index - 5] : '')
+  }).join('\n')
+}
+
+function load (art) {
+  return fs.readFileSync(
+    path.join(path.dirname(process.argv[1]), 'art', art)
+  , { encoding: 'utf8' }
+  )
 }
